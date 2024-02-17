2a2e59e0dd078d762b73a88edcb120d5e56a23c1
hendisantika <hendisantika@yahoo.co.id>
2024-02-17 13:47:45 +0700

    Add RoutingDataSource🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/main/java/id/my/hendisantika/multitenancy/config/RoutingDataSource.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.multitenancy.config;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jpa-multitenancy
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2/17/24
+ * Time: 13:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class RoutingDataSource extends AbstractRoutingDataSource {
+    private static final Map<Object, Object> dataSourceMap = new HashMap<>();
+
+}

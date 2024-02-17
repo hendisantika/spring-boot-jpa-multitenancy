cbeb43c8c11df0f0c16f1b201dc9a9c4021263e0
hendisantika <hendisantika@yahoo.co.id>
2024-02-17 13:38:24 +0700

    Add User Entity🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/main/java/id/my/hendisantika/multitenancy/entity/User.java
@@ -0,0 +1,36 @@
+package id.my.hendisantika.multitenancy.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jpa-multitenancy
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2/17/24
+ * Time: 13:37
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@Entity
+@Table(name = "users")
+@EqualsAndHashCode(callSuper = false)
+public class User extends BaseEntity {
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "person_id")
+    private Person person;
+}

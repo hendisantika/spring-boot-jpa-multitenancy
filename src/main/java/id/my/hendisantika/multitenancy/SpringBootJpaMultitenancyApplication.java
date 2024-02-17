88735d25f0d5aef499573274a6f024e6a76d66f3
hendisantika <hendisantika@yahoo.co.id>
2024-02-17 13:17:30 +0700

    Create starter project 🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/main/java/id/my/hendisantika/multitenancy/SpringBootJpaMultitenancyApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.multitenancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaMultitenancyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJpaMultitenancyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/multitenancy/SpringBootJpaMultitenancyApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.multitenancy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaMultitenancyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
